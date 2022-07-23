d47325a09ae79dcac0e1620c47083a14ae9f2a87
Priyavrat Sharma <Priyavrat18@gmail.com>
2022-07-23 20:50:36 +0200

    Added Junit tests

--- src/test/java/com/priyavrat/Restful/Controller/UserControllerTest.java
+++ src/test/java/com/priyavrat/Restful/Controller/UserControllerTest.java
@@ -9,18 +9,21 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,21 +46,48 @@ class UserControllerTest {
     User USER3= new User(3,"Richu", LocalDateTime.of(1994, 8, 5, 10, 32, 14, 640000));
 
     @Test
-    void retrieveAllUsers() throws Exception {
+    void retrieveAllUsersTest() throws Exception {
         List<User> users= new ArrayList<>(Arrays.asList(USER1,USER2,USER3));
         Mockito.when(userRepository.findAll()).thenReturn(users);
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/users")
+        mockMvc.perform(MockMvcRequestBuilders.get("/users-rest/users")
                 .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
                 .andExpect(jsonPath("$",hasSize(3)))
                 .andExpect(jsonPath("$[2].name",is("Richu")));
     }
 
     @Test
-    void retrieveUser() {
+    void retrieveUserTest() throws Exception {
+        Mockito.when(userRepository.findById(USER1.getId())).thenReturn(Optional.of(USER1));
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/users-rest/users/1")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",notNullValue()))
+                .andExpect(jsonPath("$.name",is("Priyavrat")));
     }
 
     @Test
-    void createUser() {
+    void createUserTest() throws Exception {
+        User user = User.builder().id(1).name("Priyavrat").birthDate(LocalDateTime.of(1994, 3, 5, 10, 32, 12, 640000)).build();
+        Mockito.when(userRepository.save(user)).thenReturn(user);
+        MockHttpServletRequestBuilder mockRequest =MockMvcRequestBuilders.post("/users-rest/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(this.mapper.writeValueAsString(user));
+        mockMvc.perform(mockRequest)
+                .andExpect(status().is(201));
     }
+
+    @Test
+    void deleteUserTest() throws Exception{
+        Mockito.when(userRepository.findById(USER2.getId())).thenReturn(Optional.of(USER2));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                .delete("/users-rest/users/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+
 }
